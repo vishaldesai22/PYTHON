9c659815b64410dc176c7bf47a1e6fbeef67f81a
vishaldesai22 <155609881+vishaldesai22@users.noreply.github.com>
2024-04-24 11:38:27 +0530

    Add files via upload

---
+++ implicit and explicit fun.py
@@ -0,0 +1,40 @@
+# implicit function 
+'''a=10
+b=20.20
+c= a+b
+print(c)
+'''
+
+#explicit function
+'''x=10
+#print (x)
+q=float(x)
+print(x)
+a= type(q)
+print(a)
+print(q)
+'''
+
+#eval function
+'''w= eval('1+2')
+print(w)
+'''
+
+#unichr
+'''a=65
+o=unichr(a)
+print(o)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           i
+'''
+
+#hexadecimal
+'''s=hex(12)
+print(s)
+'''
+
+#octal
+'''d= oct (6)
+print(d)
+'''
+
+#
+
