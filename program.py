89f9c8385735fc30842002662e374a3a7f4454a8
vishaldesai22 <155609881+vishaldesai22@users.noreply.github.com>
2024-04-24 11:38:55 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,28 @@
+import math
+w=math.ceil(12.12)
+print (w)
+
+f=math.floor(12.99)
+print(f)
+
+
+c= math.cos(90)
+print(c)
+
+e= math.exp(3)
+print(e)
+
+m= math.factorial(5)
+print(m)
+
+g=math.fmod(50,10)
+print (g)
+
+d=math.sqrt(30)
+print(d)
+
+t= math.trunc(12.4444)
+print(t)
+
+s=math.copysign(-10,12)
+print(s)
