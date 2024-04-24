9fc37b3d92b06084596b87c8ec2844fd6e2b5a90
vishaldesai22 <155609881+vishaldesai22@users.noreply.github.com>
2024-04-24 11:35:58 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,18 @@
+
+#specific fun.
+from example import add
+print(add(12,12))
+
+
+
+#multiple function
+from example import add,sub,mul
+print(add(13,14))
+print (sub(44,33))
+print(mul(10,1))
+
+#all the functions
+
+from example import *
+print(div(10,5))
+
